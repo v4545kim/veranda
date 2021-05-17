bff88e2fd892d8dee78eddfbea952e9ac68f6e29
v4545kim <v4545kim@naver.com>
2021-05-17 10:06:38 +0900

    commit

--- src/com/veranda/member/controller/MemberNickcheckController.java
+++ src/com/veranda/member/controller/MemberNickcheckController.java
@@ -16,19 +16,21 @@ public class MemberNickcheckController extends SuperClass{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
 		
-		String nickname = request.getParameter("user_nickname");
+		System.out.println("MemberNickCheckController 가 시작되었습니다.");
+	
+		String nickname = request.getParameter("nickname");
 		
 		MemberDao dao = new MemberDao();
 		
 		Member bean = dao.SelectDataByPk(nickname);
 		
-		if ( bean == null ) { /* DB에 동일한 아이디가 없다면 */
+		if ( bean == null ) { /* DB에 동일한 닉네임이 없다면 */
 			request.setAttribute("message",  nickname + "<font color='blue')은(는) <b> 사용 가능 </b>합니다!</font>");
 			request.setAttribute("nickisCheck", true);
 			
 		} 
 		
-		String gotopage = "/member/nickCheck.jsp";
+		String gotopage = "/members/nickNameCheck.jsp";
 		super.GotoPage(gotopage);
 	}
 	
